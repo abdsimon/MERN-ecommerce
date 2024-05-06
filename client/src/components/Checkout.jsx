90b2e349311ceb84ff3e4047d1eba5338f04172e
Simon <diababdulwahed@gmail.com>
2024-05-06 16:06:14 +0200

    header.jsx

---
+++ client/src/components/Checkout.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+          <li><Link to="/checkout">Checkout</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
