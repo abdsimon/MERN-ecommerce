b70ac7407b2eb6de789701d1a0bb0f65433cf7c4
Simon <diababdulwahed@gmail.com>
2024-05-07 12:47:14 +0200

    Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our E-commerce Store!</h1>
+      <p>
+        Browse through our collection of amazing products. 
+        Click on any product to view details or add it to your cart.
+      </p>
+      
+    </div>
+  );
+}
+
+export default Home;
