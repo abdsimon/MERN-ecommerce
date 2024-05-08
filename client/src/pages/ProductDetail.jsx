e1b2f0e143a10ed8a5a65de3664acb1e7eb0401e
Simon <diababdulwahed@gmail.com>
2024-05-08 19:41:20 +0200

    changes

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -12,7 +12,7 @@ import axios from 'axios';
     <div>
       <h2>{product.name}</h2>
       <p>{product.description}</p>
-   
+      <p>Price: ${product.price}</p>
       {/* Add functionality to add this product to cart */}
 
     </div>
