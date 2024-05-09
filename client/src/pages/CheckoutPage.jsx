493306a62783a0f80b5af47bf0b58293e2756dec
Simon <diababdulwahed@gmail.com>
2024-05-09 17:15:04 +0200

    changes

---
+++ client/src/pages/CheckoutPage.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+
+
+
+  
+
+
+
+  return (
+    <div>
+      <h2>Checkout</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+        <label>Email:</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        <label>Address:</label>
+        <textarea name="address" value={formData.address} onChange={handleChange} required />
+        <button type="submit">Place Order</button>
+      </form>
+      <p>Total: ${totalPrice}</p>
+    </div>
+  );
+}
+
+export default CheckoutPage;
