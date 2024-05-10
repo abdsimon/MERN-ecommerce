140d68acdaa5ee1c75d6b91c6397278bc8c5d2ce
Simon <diababdulwahed@gmail.com>
2024-05-10 20:20:44 +0200

    chang

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -7,11 +7,11 @@ const ProductDetail = ({ product }) => {
 
   return (
     <div>
-  
-      
-      
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
  
-      
+      <button>Add to Cart</button>
     </div>
   );
 }
