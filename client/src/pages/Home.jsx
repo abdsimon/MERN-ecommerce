c0a9f6a23d3d28c6958eb047701721fe1501179e
Simon <diababdulwahed@gmail.com>
2024-05-12 23:35:58 +0200

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Our E-commerce Store!</h1>
+      <h1>Welcome to Our E-commerce Store</h1>
       <p>
         Browse through our collection of amazing products. 
         Click on any product to view details or add it to your cart.
