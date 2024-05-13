906b95415955ec628b1948ee99327cc8677f4e29
Simon <diababdulwahed@gmail.com>
2024-05-13 21:04:11 +0200

    changes

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -5,7 +5,13 @@ import React, { useState } from 'react';
 
   return (
   
-          
+            {cartItems.map(item => (
+              <li key={item.id}>
+                <span>{item.name}</span>
+                <span>Quantity: {item.quantity}</span>
+                <span>Price: ${item.price}</span>
+                <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
+              </li>
             ))}
           </ul>
           <p>Total: ${totalPrice}</p>
