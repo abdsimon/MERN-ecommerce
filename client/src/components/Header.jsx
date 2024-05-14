4ded8e01473a255eadbb4ecb5661f136ed256b03
Simon <diababdulwahed@gmail.com>
2024-05-14 13:59:35 +0200

    Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Header = () => {
   return (
- 
+    <header>
+      <h1>My E-commerce Store</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/cart">Cart</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
