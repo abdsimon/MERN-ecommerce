474ad8f9f7778c2202ced76ed8e7d1e3678dee26
Simon <diababdulwahed@gmail.com>
2024-05-20 20:23:03 +0200

    changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>My E-com Store</h1>
+      <h1>My E-cog Store</h1>
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
