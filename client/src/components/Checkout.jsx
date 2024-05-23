a1e3f6a952fb39778a2a62095fa44c4cc828276e
Simon <diababdulwahed@gmail.com>
2024-05-23 19:47:11 +0200

    changes

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/cart">Cart</Link></li>
-          <li><Link to="/checkout">Chec</Link></li>
+          <li><Link to="/checkout">Che</Link></li>
         </ul>
       </nav>
     </header>
