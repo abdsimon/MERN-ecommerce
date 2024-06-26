4bfd948421d5da6a93e363d5075487281f251e43
Simon <diababdulwahed@gmail.com>
2024-06-26 17:10:18 +0200

    cxhanges

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p> 2024 My Ecommerce website  mo </p>
+      <p> 2024 My Ecommerce website  m </p>
     </footer>
   );
 };
